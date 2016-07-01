ecccb3b76823de5be27f7b82d90235e77ce93889
Brian McGehee <doc@tavian.com>
2016-07-01 14:57:18 -0700

    second

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM ubuntu:14.04
 RUN apt-get update -y
 RUN sudo apt-get -y install nodejs
 COPY . /src
-EXPOSE 3000
+EXPOSE 3001
 CMD ["nodejs", "/src/app.js"]
--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(function(req, res, next) {
     next(err);
 });
 
-var server = app.listen(process.env.PORT || 3000, function (){
+var server = app.listen(process.env.PORT || 3001, function (){
   var port = server.address().port;
   console.log('Magic is happening on port', port, " at ", timeController.getDate());
 });
